02e62d289e63fbe9ebb1331df50851cc55de4280
kylanaval <kylanaval@gmail.com>
2026-09-23 16:48:30 -0600

    Add welcome screen with introductory message for Acme Accounting Systems
    - Displays a welcome message and a tagline in the console application.
    - Sets the foundation for the Acme Accounting Systems project.

--- Welcome Screen/Welcome Screen/Program.cs
+++ Welcome Screen/Welcome Screen/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to Acme Accounting Systems");
+        Console.WriteLine("Remember, we’re “accounting” on you!");
+    }
+}
