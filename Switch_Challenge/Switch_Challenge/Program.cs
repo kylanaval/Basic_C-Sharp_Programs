fe09394b3707e7ee9a9ff981a7eb13487b77eebd
kylanaval <kylanaval@gmail.com>
2026-09-24 00:16:41 -0600

    This C# console application prompts the user to select a day of the week (Monday to Friday) by entering a number (1–5) and displays the corresponding day using a switch statement. If the input is invalid, it notifies the user.

---
+++ Switch_Challenge/Switch_Challenge/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Choose a day of the week:");
+        Console.WriteLine("1 - Monday");
+        Console.WriteLine("2 - Tuesday");
+        Console.WriteLine("3 - Wednesday");
+        Console.WriteLine("4 - Thursday");
+        Console.WriteLine("5 - Friday");
+
+        Console.Write("Enter your choice: ");
+        int day = Convert.ToInt32(Console.ReadLine());
+
+        switch (day)
+        {
+            case 1:
+                Console.WriteLine("You chose Monday.");
+                break;
+
+            case 2:
+                Console.WriteLine("You chose Tuesday.");
+                break;
+
+            case 3:
+                Console.WriteLine("You chose Wednesday.");
+                break;
+
+            case 4:
+                Console.WriteLine("You chose Thursday.");
+                break;
+
+            case 5:
+                Console.WriteLine("You chose Friday.");
+                break;
+
+            default:
+                Console.WriteLine("Invalid choice.");
+                break;
+        }
+    }
+}
