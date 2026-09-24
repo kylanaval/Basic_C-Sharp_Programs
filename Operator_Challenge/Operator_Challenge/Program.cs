80b7b2f009b6848f37b2d542446f29d6658fb763
kylanaval <kylanaval@gmail.com>
2026-09-23 23:29:37 -0600

    This program compares two integer values and displays whether they are equal or not.

---
+++ Operator_Challenge/Operator_Challenge/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int num1 = 10;
+        int num2 = 20;
+
+        Console.WriteLine("Welcome to the Operator Challenge!");
+        Console.WriteLine("This program will compare two numbers and determine if they are equal or not.");
+        Console.WriteLine();
+        Console.WriteLine("Number 1: " + num1);
+        Console.WriteLine("Number 2: " + num2);
+
+        if (num1 != num2)
+        {
+            Console.WriteLine("The numbers are not equal.");
+        }
+        else
+        {
+            Console.WriteLine("The numbers are equal.");
+        }
+
+        Console.ReadLine();
+    }
+}
