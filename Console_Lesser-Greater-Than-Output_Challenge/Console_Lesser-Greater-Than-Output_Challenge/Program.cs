95a2e55021f1d7304ee0efd6296b3b06e2666580
kylanaval <kylanaval@gmail.com>
2026-09-23 23:34:23 -0600

    This C# program demonstrates the use of the greater than (>) and less than or equal to (<=) comparison operators. It compares integer values, evaluates the results as boolean values, and outputs the comparisons and their results to the console.

---
+++ Console_Lesser-Greater-Than-Output_Challenge/Console_Lesser-Greater-Than-Output_Challenge/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using static System.Net.Mime.MediaTypeNames;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Greater than operator
+        int num1 = 10;
+        int num2 = 5;
+
+        bool greaterResult = num1 > num2;
+
+        Console.WriteLine("Greater Than Operator:");
+        Console.WriteLine(num1 + " > " + num2 + " = " + greaterResult);
+
+        Console.WriteLine();
+
+        // Less than or equal to operator
+        int num3 = 5;
+        int num4 = 5;
+
+        bool lessOrEqualResult = num3 <= num4;
+
+        Console.WriteLine("Less Than or Equal To Operator:");
+        Console.WriteLine(num3 + " <= " + num4 + " = " + lessOrEqualResult);
+
+        Console.ReadLine();
+    }
+}
