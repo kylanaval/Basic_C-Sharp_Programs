e67370d562bb805cbc79be4dc3c01c96e5268b50
kylanaval <kylanaval@gmail.com>
2026-09-23 21:03:35 -0600

    Math Challenge is a simple C# console application that performs basic arithmetic operations (addition, subtraction, multiplication, division) on two predefined numbers.

---
+++ Math_Challenge/Math_Challenge/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to the Math Challenge!");
+        Console.WriteLine("This program will perform basic arithmetic operations on two numbers.");
+        Console.WriteLine("The number1 is 20 and number2 is 5.");
+        Console.WriteLine();
+
+        // Two numbers
+        double number1 = 20;
+        double number2 = 5;
+
+        // Addition
+        double addition = number1 + number2;
+        Console.WriteLine("Addition: " + addition);
+
+        // Subtraction
+        double subtraction = number1 - number2;
+        Console.WriteLine("Subtraction: " + subtraction);
+
+        // Multiplication
+        double multiplication = number1 * number2;
+        Console.WriteLine("Multiplication: " + multiplication);
+
+        // Division
+        double division = number1 / number2;
+        Console.WriteLine("Division: " + division);
+
+        Console.ReadLine();
+    }
+}
