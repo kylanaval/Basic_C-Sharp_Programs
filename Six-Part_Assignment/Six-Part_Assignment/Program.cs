115ccac7130bb67cac4b733fafc2c2a9c8dfed73
kylanaval <kylanaval@gmail.com>
2026-09-25 00:01:43 -0600

    Demonstrates string concatenation and uppercase conversion in C#.

---
+++ Six-Part_Assignment/Six-Part_Assignment/Program.cs
@@ -0,0 +1,15 @@
+﻿//Assignment Part1
+//Three strings to concatenate:
+string lineOne = "Beware, ";
+string lineTwo = "for I am fearless, ";
+string lineThree = "and therefore powerful.";
+
+//Concatenate the three strings:
+Console.WriteLine(lineOne + lineTwo + lineThree);
+
+//Assignment Part2
+//A string to be converted to uppercase:
+string x = "who controls the past controls the future.";
+
+//Convert the string to uppercase:
+Console.WriteLine(x.ToUpper());
